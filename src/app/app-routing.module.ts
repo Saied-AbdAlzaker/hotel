1846bd9ce0997aad4134fd487be2518d47e4c3d9
Saied-AbdAlzaker <saiedabdalzaker@gmail.com>
2024-01-01 20:33:35 +0200

    Project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeModule } from './Core/Pages/home/home/home.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: 'home', pathMatch:'full'},
+  {path: 'home',
+  loadChildren: () => import('./Core/Pages/home/home/home.module').then(m => m.HomeModule)},
+  {path: 'auth',
+  loadChildren: () => import('./Core/auth/auth.module').then(m => m.AuthModule)},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Core/auth/Components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    
   ],
   imports: [
     BrowserModule,
