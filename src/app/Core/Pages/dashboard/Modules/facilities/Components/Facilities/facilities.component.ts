39c68f09b33dcd574b934fa4a94ff9bf7adf759b
Ahmed-Adel33 <ahmed920adel@gmail.com>
2024-01-06 19:01:56 +0200

    f

--- src/app/Core/Pages/dashboard/Modules/facilities/Components/Facilities/facilities.component.ts
+++ src/app/Core/Pages/dashboard/Modules/facilities/Components/Facilities/facilities.component.ts
@@ -1,19 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RoomsService } from '../../../rooms/services/rooms.service';
+import { IFacilities } from '../../../rooms/model/room';
 
 @Component({
   selector: 'app-facilities',
   templateUrl: './facilities.component.html',
   styleUrls: ['./facilities.component.scss']
 })
-export class FacilitiesComponent {
-  tableData:any;
+export class FacilitiesComponent implements OnInit {
+  facilities: IFacilities[]=[];
+
   pageIndex: number = 0;
   pageSize: number = 10;
   pageNumber: number | undefined = 1;
   imagePath:string = 'http://upskilling-egypt.com:3000/';
+  constructor(private _roomService:RoomsService){}
 
   
   ngOnInit() {
+    this.getFacilities();
     // this.getAllRooms();
     // this.getFacilities();
 
@@ -52,6 +57,17 @@ export class FacilitiesComponent {
   // onSearchInputChange() {
   //   this.searchSubject.next(this.searchValue);
   // }
+ 
+
+  getFacilities(){
+    this._roomService.onGetFacilities().subscribe({
+      next:(res:any)=>{
+        console.log(res);
+        this.facilities=res.data.facilities
+      }
+    })
+  }
+
   handlePageEvent(e: any) {
     this.pageSize = e.pageSize;
     this.pageNumber = e.pageIndex + 1;
