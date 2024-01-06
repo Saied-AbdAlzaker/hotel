1b241560aa385cf5a1e9da381fc136a01ab4cb15
Fatma Ashraf <129579797+FattemaAshraf@users.noreply.github.com>
2024-01-06 17:45:24 +0200

    enhancement delete-dialog room bug

--- src/app/Core/Pages/dashboard/Modules/rooms/rooms.module.ts
+++ src/app/Core/Pages/dashboard/Modules/rooms/rooms.module.ts
@@ -10,8 +10,6 @@ const routes: Routes = [
   {path: '', component: RoomsComponent},
   {path: 'add', component: AddEditRoomComponent},
   {path: 'edit/:_id', component: AddEditRoomComponent},
-
-
 ]
 
 @NgModule({
