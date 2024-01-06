68a4657aa3191c972794285b8610b7dc39f38b38
Ahmed-Adel33 <ahmed920adel@gmail.com>
2024-01-06 16:47:08 +0200

    f

--- src/app/Core/Pages/dashboard/Modules/facilities/facilities.module.ts
+++ src/app/Core/Pages/dashboard/Modules/facilities/facilities.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FacilitiesRoutingModule } from './facilities-routing.module';
-import { FacilitiesComponent } from './Components/facilities.component';
-import { AddEditComponent } from './Components/Facilities/add-edit/add-edit.component';
+import { FacilitiesComponent } from './Components/Facilities/facilities.component';
 
 
 @NgModule({
   declarations: [
-    FacilitiesComponent,
-    AddEditComponent
+    FacilitiesComponent
   ],
   imports: [
     CommonModule,
