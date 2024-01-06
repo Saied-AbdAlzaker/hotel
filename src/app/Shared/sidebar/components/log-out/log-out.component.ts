65ca90637a68932d208cd48d3e77f3b0df8f7706
Fatma Ashraf <129579797+FattemaAshraf@users.noreply.github.com>
2024-01-06 15:17:41 +0200

    added angular material

--- src/app/Shared/sidebar/components/log-out/log-out.component.ts
+++ src/app/Shared/sidebar/components/log-out/log-out.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-log-out',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./log-out.component.scss']
 })
 export class LogOutComponent {
-
+  constructor(
+    private Router:Router,
+    public dialog: MatDialog
+     ) {}
+logout(){
+  localStorage.removeItem('userToken');
+  localStorage.removeItem('role');
+  localStorage.removeItem('userName');
+  localStorage.removeItem('userEmail')
+  localStorage.removeItem('loglevel')
+  this.Router.navigate(['/auth'])
+}
 }
