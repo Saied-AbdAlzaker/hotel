a16eb250dc415ee0e39b9f834f549c26cd6bd288
sara-essam-ata <samsarh194@gmail.com>
2024-01-07 04:21:58 +0200

    solve pagination conflict

--- src/app/Core/Pages/dashboard/Modules/users/components/users/users.component.ts
+++ src/app/Core/Pages/dashboard/Modules/users/components/users/users.component.ts
@@ -9,13 +9,6 @@ import { IlistTable, IlistUser } from '../../model/users';
 })
 export class UsersComponent implements OnInit {
   pageIndex: number = 0;
-
-pageNumber:number=1;
-pageSize:number=10;
-listUses:IlistUser[]|undefined=[];
-tableResponse:IlistTable|undefined;
-  constructor(private  _UsersService:UsersService) { }
-
   pageSize: number = 10;
   pageNumber: number | undefined = 1;
   listUses: IlistUser[] | undefined = [];
@@ -45,28 +38,5 @@ tableResponse:IlistTable|undefined;
     this.onGetAllUsers();
   }
 
-  onGetAllUsers() {
-
-    let params = {
-      pageNumber: this.pageNumber,
-      pageSize: this.pageSize
-    }
-    this._UsersService.geAllUsers(params).subscribe({
-      next: (res: any) => {
-        this.tableResponse = res.data;
-
-        this.listUses = res.data.users;
-
-
-      }
-    })
-  }
-
-  handlePageEvent(e: any) {
-    this.pageSize = e.pageSize;
-    this.pageNumber = e.pageIndex + 1;
-
-  }
-
-
+ 
 }
