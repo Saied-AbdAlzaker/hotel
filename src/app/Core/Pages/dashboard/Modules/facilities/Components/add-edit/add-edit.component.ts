2a2150a893eaa1eb0d4f09fbc04a395510bc7077
Saied-AbdAlzaker <saiedabdalzaker@gmail.com>
2024-01-07 20:19:17 +0200

    update

--- src/app/Core/Pages/dashboard/Modules/facilities/Components/add-edit/add-edit.component.ts
+++ src/app/Core/Pages/dashboard/Modules/facilities/Components/add-edit/add-edit.component.ts
@@ -8,15 +8,8 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 })
 export class AddEditComponent implements OnInit {
 
-  facilitiesName: string = '';
-  isUpdatePage: boolean = false;
-  isLoading: boolean = false;
-  FacilitisId: any;
-
   constructor(public dialogRef: MatDialogRef<AddEditComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) {
-
-  }
+    @Inject(MAT_DIALOG_DATA) public data: any) {}
 
   onNoClick(): void {
     this.dialogRef.close();
