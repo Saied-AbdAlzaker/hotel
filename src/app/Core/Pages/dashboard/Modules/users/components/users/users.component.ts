a9e31f1ab9c7a33082e3db3464bd5a7d1c48c67f
Ahmed-Adel33 <ahmed920adel@gmail.com>
2024-01-09 22:52:20 +0200

    listusers

--- src/app/Core/Pages/dashboard/Modules/users/components/users/users.component.ts
+++ src/app/Core/Pages/dashboard/Modules/users/components/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../../services/users.service';
+import { IlistTable, IlistUser } from '../../model/users';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
-
-  constructor() { }
+pageNumber:number=1;
+pageSize:number=10;
+listUses:IlistUser[]|undefined=[];
+tableResponse:IlistTable|undefined;
+  constructor(private  _UsersService:UsersService) { }
 
   ngOnInit() {
+    this.onGetAllUsers();
   }
+  onGetAllUsers(){
+
+let params={
+  pageNumber:this.pageNumber,
+  pageSize:this.pageSize  
+}
+    this._UsersService.geAllUsers(params).subscribe({
+      next:(res:any)=>{
+        this.tableResponse=res.data;
+
+      this.listUses=res.data.users;
+      
+        
+      }
+    })
+  }
+ 
 
 }
--- src/app/Core/Pages/dashboard/Modules/users/model/users.ts
+++ src/app/Core/Pages/dashboard/Modules/users/model/users.ts
@@ -1,2 +1,18 @@
-export interface Users {
+export interface IlistTable {  
+    totalCount: number,
+       listuser:IlistUser[]
+}
+
+export interface IlistUser {
+        _id:string,
+        userName: string,
+        email: string,
+        phoneNumber: number,
+        country: string ,
+        role: string,
+        verified: boolean,
+        createdAt: string,
+        updatedAt: string
+    
+
 }
