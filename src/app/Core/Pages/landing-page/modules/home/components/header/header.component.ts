8edc20f41c833af7f0673cfb122cdcbaecb774ff
sara-essam-ata <samsarh194@gmail.com>
2024-01-09 14:29:09 +0200

    header

--- src/app/Core/Pages/landing-page/modules/home/components/header/header.component.ts
+++ src/app/Core/Pages/landing-page/modules/home/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HelperService } from 'src/app/Core/services/helper.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-
+  constructor(public _HelperService:HelperService){}
 }
