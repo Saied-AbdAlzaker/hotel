e656164ac7d33e07f0fa3376d745e93c53d6944f
Ahmed-Adel33 <ahmed920adel@gmail.com>
2024-01-16 23:00:33 +0200

    g

--- src/app/Core/Pages/landing-page/modules/home/home.module.ts
+++ src/app/Core/Pages/landing-page/modules/home/home.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './components/home/home.component';
 import { SharedModule } from 'src/app/Shared/shared.module';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
   ],
   imports: [
     CommonModule,
