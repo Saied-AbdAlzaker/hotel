047d0f0b5113c6d521fb09ee30e005eddffe9ccc
Fatma Ashraf <129579797+FattemaAshraf@users.noreply.github.com>
2024-01-19 19:20:59 +0200

    added most-picked integration

--- src/app/Core/Pages/landing-page/modules/home/models/home.ts
+++ src/app/Core/Pages/landing-page/modules/home/models/home.ts
@@ -1,2 +1,22 @@
-export interface Home {
+import { IFacilities } from 'src/app/Core/Pages/dashboard/Modules/rooms/model/room';
+
+export interface IRoomsUserDetails {
+  page: number;
+  size: number;
+  data: {
+    rooms: IRoomsUser[];
+  totalCount: number
+  };
+}
+export interface IRoomsUser {
+  _id: string;
+  roomNumber: string;
+  images: string[];
+  price: number;
+  capacity: number;
+  discount: number;
+  facilities: IFacilities[];
+  createdAt: Date;
+  updatedAt: Date;
+  isBooked: boolean;
 }
