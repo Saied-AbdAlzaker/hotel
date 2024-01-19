60c15df72cd52df9ad6ac50af8edc8eb82130b93
Ahmed-Adel33 <ahmed920adel@gmail.com>
2024-01-19 20:52:34 +0200

    d

--- src/app/Core/Pages/landing-page/modules/home/components/features/features.component.ts
+++ src/app/Core/Pages/landing-page/modules/home/components/features/features.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HomeService } from '../../services/home.service';
+import { IRoomsUser, IRoomsUserDetails } from '../../models/home';
 
 @Component({
   selector: 'app-features',
   templateUrl: './features.component.html',
   styleUrls: ['./features.component.scss']
 })
-export class FeaturesComponent {
+export class FeaturesComponent implements OnInit {
+  size:number=10;
+  page:number=1;
+  roomResponse:IRoomsUserDetails|undefined;
+  rooms: IRoomsUser[] | any;
 
+constructor(private _HomeService:HomeService){}
+  ngOnInit(): void {
+    this.getAllRooms();
+  }
+  getAllRooms(){
+    let params={
+       size:this.size,
+       page:this.page
+    }
+    this._HomeService.getAllRooms(params).subscribe({
+      next:(res)=>{
+        console.log(res);
+        this.roomResponse=res.data;
+        this.rooms=this.roomResponse?.data.rooms;
+        
+      }     
+    })
+  }
  
 }
