1139df9b43804b6ac10a0b28c80f52702d05c874
Fatma Ashraf <129579797+FattemaAshraf@users.noreply.github.com>
2024-01-20 10:23:07 +0200

    added remove favourite itegration

--- src/app/Core/Pages/landing-page/modules/home/components/favourites/favourites.component.ts
+++ src/app/Core/Pages/landing-page/modules/home/components/favourites/favourites.component.ts
@@ -5,6 +5,9 @@ import {
   IFavouriteResponse,
   IFavouriteRooms,
 } from '../../models/ifavourite';
+import { ToastrService } from 'ngx-toastr';
+import { DeleteDialogComponent } from 'src/app/Shared/delete-dialog/delete-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-favourites',
@@ -15,7 +18,11 @@ export class FavouritesComponent implements OnInit {
   favouritesResponse: IFavouriteResponse | undefined;
   favouriteData: IFavouriteData | undefined;
   favouriteItems: IFavouriteRooms | any;
-  constructor(private _favouriteService: FavouriteService) {}
+  constructor(
+    private _favouriteService: FavouriteService,
+    private dialog: MatDialog,
+    private _toastrService: ToastrService
+  ) {}
   ngOnInit(): void {
     this.getAllFavourites();
   }
@@ -27,6 +34,39 @@ export class FavouritesComponent implements OnInit {
         this.favouriteData = this.favouritesResponse?.data;
         this.favouriteItems = this.favouriteData?.favoriteRooms[0].rooms;
         console.log(this.favouriteItems);
+      },
+    });
+  }
+  // Delete
+  openDeleteDialog(roomData: any): void {
+    console.log(roomData);
+
+    const dialogRef = this.dialog.open(DeleteDialogComponent, {
+      data: roomData,
+      width: '30%',
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+      if (result) {
+        console.log(result);
+        this.deleteFavouriteById(result._id);
+      }
+    });
+  }
+  deleteFavouriteById(roomId: any) {
+    this._favouriteService.deleteFavourite(roomId).subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: (err) => {
+        this._toastrService.error(err.error.message, '');
+      },
+      complete: () => {
+        this._toastrService.success(
+          'Room deleted from favourites successfully'
+        );
+        this.getAllFavourites();
       },
     });
   }
--- src/app/Core/Pages/landing-page/modules/home/services/favourite.service.ts
+++ src/app/Core/Pages/landing-page/modules/home/services/favourite.service.ts
@@ -3,18 +3,20 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FavouriteService {
-
   constructor(private _HttpClient: HttpClient) {}
-
 
   getMyFavourites(): Observable<any> {
     return this._HttpClient.get('portal/favorite-rooms');
   }
 
-  addFavourite(roomId:string): Observable<any> {
-    return this._HttpClient.post('portal/favorite-rooms',roomId);
+  addFavourite(roomId: string): Observable<any> {
+    return this._HttpClient.post('portal/favorite-rooms', roomId);
+  }
+  deleteFavourite(roomId: any): Observable<any> {
+    return this._HttpClient.delete(`portal/favorite-rooms/${roomId}`);
+
   }
 }
