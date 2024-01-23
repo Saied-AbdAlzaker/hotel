c228facc975ede3710e0e25dc397d376f48f9e57
sara-essam-ata <samsarh194@gmail.com>
2024-01-23 14:59:32 +0200

    header conflict

--- src/app/Core/Pages/landing-page/modules/booking/booking.module.ts
+++ src/app/Core/Pages/landing-page/modules/booking/booking.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookingRoutingModule } from './booking-routing.module';
 import { BookingComponent } from './components/booking/booking.component';
-// import { StripeElementsDirective } from 'ngx-stripe'
-// import { NgxStripeModule } from 'ngx-stripe';
 import { NgxStripeModule } from 'ngx-stripe';
 import { StripeElementsDirective,StripePaymentElementComponent } from 'ngx-stripe'
 
@@ -15,18 +13,13 @@ import { StripeElementsDirective,StripePaymentElementComponent } from 'ngx-strip
   ],
   imports: [
     CommonModule,
-    
     BookingRoutingModule,
-    // StripeElementsDirective,
-    // NgxStripeModule.forRoot('pk_test_51OTjURBQWp069pqTmqhKZHNNd3kMf9TTynJtLJQIJDOSYcGM7xz3DabzCzE7bTxvuYMY0IX96OHBjsysHEKIrwCK006Mu7mKw8'),
-
-   ]
     BookingRoutingModule,
     StripeElementsDirective,
     NgxStripeModule.forRoot(
       'pk_test_51OTjURBQWp069pqTmqhKZHNNd3kMf9TTynJtLJQIJDOSYcGM7xz3DabzCzE7bTxvuYMY0IX96OHBjsysHEKIrwCK006Mu7mKw8'
     ),
-    StripePaymentElementComponent
+    StripePaymentElementComponent,
   ]
 
 })
